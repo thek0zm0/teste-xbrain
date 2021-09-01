cc770466be3c12f8ba9c63b0a89b35ac9fc4f454
Lucas Moraes <lucasmoraes@alunos.utfpr.edu.br>
2021-09-01 15:03:50 -0300

    H2 database and Seller JPA mapping.

--- src/main/java/com/xbrainteste/testebackend/entities/Seller.java
+++ src/main/java/com/xbrainteste/testebackend/entities/Seller.java
@@ -1,13 +1,20 @@
 package com.xbrainteste.testebackend.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class Seller implements Serializable
 {
     // Serializable
     private static final long serialVersionUID = -3631374094216017714L;
     // Basic Attributes
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
