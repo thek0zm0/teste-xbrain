553cef2b6972f0e13348233b3f54b877c82ffdb2
Lucas Moraes <lucasmoraes@alunos.utfpr.edu.br>
2021-09-01 11:16:21 -0300

    Project created

---
+++ src/main/java/com/xbrainteste/testebackend/TesteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.xbrainteste.testebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteBackendApplication.class, args);
+	}
+
+}
